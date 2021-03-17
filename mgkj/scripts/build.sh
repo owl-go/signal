44a89248dcd8c45b86aa64203b36df9459ef437e
gear.ghost <cloudeniac@gmail.com>
2021-03-17 20:41:14 +0800

    add individual module build script

--- mgkj/scripts/build.sh
+++ mgkj/scripts/build.sh
@@ -4,10 +4,14 @@ APP_DIR=$(cd `dirname $0`/../; pwd)
 echo "$APP_DIR"
 cd $APP_DIR
 
+export GOPROXY="https://goproxy.cn,direct"
+
 BIZ_BIN=biz
 DIST_BIN=dist
 ISLB_BIN=islb
 SFU_BIN=sfu
+
+PROJECT=$1
 
 BUILD_PATH1=$APP_DIR/bin/$BIZ_BIN
 BUILD_PATH2=$APP_DIR/bin/$DIST_BIN
@@ -17,8 +21,41 @@ BUILD_PATH4=$APP_DIR/bin/$SFU_BIN
 help(){
     echo ""
     echo "build script"
+    echo "Usage: ./build.sh biz|dist|islb|sfu"
     echo "Usage: ./build.sh [-h]"
     echo ""
+}
+
+build_biz()
+{
+    echo "------------------build $BIZ_BIN------------------"
+    echo "go build -o $BUILD_PATH1"
+    cd $APP_DIR/cmd/biz
+    go build -tags netgo -o $BUILD_PATH1
+}
+
+build_dist()
+{
+    echo "------------------build $DIST_BIN------------------"
+    echo "go build -o $BUILD_PATH2"
+    cd $APP_DIR/cmd/dist
+    go build -tags netgo -o $BUILD_PATH2
+}
+
+build_islb()
+{
+    echo "------------------build $ISLB_BIN------------------"
+    echo "go build -o $BUILD_PATH3"
+    cd $APP_DIR/cmd/islb
+    go build -tags netgo -o $BUILD_PATH3
+}
+
+build_sfu()
+{
+    echo "------------------build $SFU_BIN------------------"
+    echo "go build -o $BUILD_PATH4"
+    cd $APP_DIR/cmd/sfu
+    go build -tags netgo -o $BUILD_PATH4
 }
 
 while getopts "o:h" arg
@@ -37,6 +74,12 @@ do
     esac
 done
 
+if [ $# -ne 1 ]
+then
+    help
+    exit 1
+fi
+
 if [[ "$OS_TYPE" == "Darwin" || "$OS_TYPE" == "darwin" || "$OS_TYPE" == "mac" ]];then
     echo "GO Target Arch: " $OS_TYPE
     export CGO_ENABLED=0
@@ -49,25 +92,26 @@ if [[ "$OS_TYPE" == "Linux" || "$OS_TYPE" == "linux" ]];then
     export GOOS=linux
 fi
 
-
-echo "------------------build $BIZ_BIN------------------"
-echo "go build -o $BUILD_PATH1"
-cd $APP_DIR/cmd/biz
-go build -tags netgo -o $BUILD_PATH1
-
-
-echo "------------------build $DIST_BIN------------------"
-echo "go build -o $BUILD_PATH2"
-cd $APP_DIR/cmd/dist
-go build -tags netgo -o $BUILD_PATH2
-
-
-echo "------------------build $ISLB_BIN------------------"
-echo "go build -o $BUILD_PATH3"
-cd $APP_DIR/cmd/islb
-go build -tags netgo -o $BUILD_PATH3
-
-echo "------------------build $SFU_BIN------------------"
-echo "go build -o $BUILD_PATH4"
-cd $APP_DIR/cmd/sfu
-go build -tags netgo -o $BUILD_PATH4
+case $PROJECT in
+$BIZ_BIN)
+    build_biz
+    ;;
+$DIST_BIN)
+    build_dist
+    ;;
+$ISLB_BIN)
+    build_islb
+    ;;
+$SFU_BIN)
+    build_sfu
+    ;;
+all)
+    build_biz
+    build_dist
+    build_islb
+    build_sfu
+    ;;
+*)
+    help
+    ;;
+esac
